fc9836623ff6caac34981d970d6164e1a9bcc4d4
Ming0099 <130200978+Ming0099@users.noreply.github.com>
2024-02-15 23:51:11 +0900

    라우팅 분리

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+var path = require('path');
+// 루트
+router.get('/',function(req, res){
+    res.sendFile(path.join(__dirname, '../public/login.html'));
+})
+
+router.get('/main',function(req, res){
+    res.sendFile(path.join(__dirname, '../public/main.html'));
+})
+
+// 회원가입
+router.get('/register',function(req, res){
+    res.sendFile(path.join(__dirname, '../public/register.html'));
+})
+
+/* 추후 웹뷰앱으로 전환 */
+// 모바일 루트
+router.get('/m',function(req, res){
+    res.sendFile(path.join(__dirname, '../public/m/main.html'));
+})
+
+// 모바일 메뉴
+router.get('/m/menu',function(req,res){
+    res.sendFile(path.join(__dirname, '../public/m/menu.html'));
+})
+
+// 모바일 로그인
+router.get('/m/login',function(req,res){
+    res.sendFile(path.join(__dirname, '../public/m/login.html'));
+})
+
+// 모바일 회원가입
+router.get('/m/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/m/register.html'));
+});
+
+// 모바일 상세정보
+router.get('/m/info',function(req,res){
+    res.sendFile(path.join(__dirname, '../public/m/info.html'));
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,17 +5,14 @@ var fs = require('fs');
 var socketio = require('socket.io');
 var admin = require("firebase-admin");
 var firestore = require("firebase-admin/firestore");
+const home = require("./routes"); // 라우팅
+const PORT = 8080;
 
 var serviceAccount = require("./vacation2023-2-firebase-adminsdk-guq8c-803226e5b1.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
-/*
-const db = firestore.getFirestore();
-db.collection('점포').doc('삼방시장').get().then((result)=>{
-    console.log(result.data());
-});*/
 
 const app = express();
 
@@ -29,47 +26,9 @@ const servers = http.createServer(credentials, app);
 
 app.use(express.static(__dirname + '/public'));
 
-// 루트
-app.get('/',function(req, res){
-    res.sendFile(__dirname + '/public/login.html');
-})
+app.use("/",home);
 
-app.get('/main',function(req, res){
-    res.sendFile(__dirname + '/public/main.html');
-})
-
-// 회원가입
-app.get('/register',function(req, res){
-    res.sendFile(__dirname + '/public/register.html');
-})
-
-/* 추후 웹뷰앱으로 전환 */
-// 모바일 루트
-app.get('/m',function(req, res){
-    res.sendFile(__dirname + '/public/m/main.html');
-})
-
-// 모바일 메뉴
-app.get('/m/menu',function(req,res){
-    res.sendFile(__dirname + '/public/m/menu.html');
-})
-
-// 모바일 로그인
-app.get('/m/login',function(req,res){
-    res.sendFile(__dirname+'/public/m/login.html');
-})
-
-// 모바일 회원가입
-app.get('/m/register', (req, res) => {
-    res.sendFile(__dirname+'/public/m/register.html');
-});
-
-// 모바일 상세정보
-app.get('/m/info',function(req,res){
-    res.sendFile(__dirname + '/public/m/info.html');
-})
-
-servers.listen(8080, function(){
+servers.listen(PORT, function(){
     console.log('Server Running at https://localhost:8080');
 })
 
